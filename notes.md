e87d28f074d9f23b2238b3897ed603a660f9b49a
Mathieu KERN <kernmathieu@gmail.com>
2018-11-15 09:38:57 +0100

    init

---
+++ notes.md
@@ -0,0 +1,13 @@
+#NFE102
+
+Angluar 
+
+## Contexte 
+
+Cr√er un front end pour aficher des film depuis une API 
+
+## Etapes 
+
+* cr√ation d'une toobar 
+* router: /, home, error
+* 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesComponent } from './movies/movies.component';
+import { RouterModule, Routes} from '@angular/router';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
+const routes: Routes = [
+  {path: '/', component : HomeComponent },
+  {path: 'movies', component : MoviesComponent},
+  {path: '**', component : ErrorComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent, 
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
